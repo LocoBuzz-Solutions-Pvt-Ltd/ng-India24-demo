cff81a4241f92365797f4a1506e146f38a414f26
Shubham Kumar Keshri <shubham.keshri@locobuzz.com>
2024-02-15 16:57:25 +0530

    Schematic added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DirectivesDemoComponent } from './components/directives-demo/directives-demo.component';
 import { DeferDemoComponent } from './components/defer-demo/defer-demo.component';
+import { HeaderComponent } from './components/schematics-demo/header/header.component';
+import { SchematicsComponent } from './components/schematics-demo/schematics/schematics.component';
 
 export const routes: Routes = [
   {
@@ -22,4 +24,8 @@ export const routes: Routes = [
     path: 'defer',
     component: DeferDemoComponent,
   },
+  {
+    path: 'schematics',
+    component: SchematicsComponent
+  }
 ];
---
+++ src/app/components/schematics-demo/schematics/schematics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schematics',
+  standalone: true,
+  imports: [],
+  templateUrl: './schematics.component.html',
+  styleUrl: './schematics.component.scss'
+})
+export class SchematicsComponent {
+
+}
